64dcec8c4c003ad3e731885cd4e4322d944726fb
RoboticRams10243 <49327777+RoboticRams10243@users.noreply.github.com>
2020-02-20 12:57:57 -0500

    potentially breaking everything by re-adding undeclared files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leagueRobot/S2RlSM.java
@@ -0,0 +1,186 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.leagueRobot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+Start with the front color sensor inline with the middle of the first stone
+ */
+
+@Autonomous(name="Two Sample Red", group="Linear Opmode")
+
+public class S2RlSM extends LinearOpMode {
+
+    private enum State{
+        STATE_INITIAL,
+        STATE_DRIVE_TO_STONE,
+        STATE_LOCATE_STONE,
+        STATE_DRIVE_TO_DUMP,
+        STATE_DUMP,
+        STATE_RETURN,
+        STATE_ACQUIRE_2STONE,
+        STATE_DRIVE_TO_LAST_DUMP,
+        STATE_PARK,
+        STATE_STOP,
+    }
+
+    private ElapsedTime mStateTime = new ElapsedTime();
+    private ElapsedTime runtime = new ElapsedTime();
+    public ColorSensor sensorColorFront = null;
+    public ColorSensor sensorColorMiddle = null;
+    private State mCurrentState; //Current State Machine State.
+    public S2RlSM(){
+
+    }
+
+
+    @Override
+    public void runOpMode()
+    {
+        drive vroom = new drive(this, telemetry, hardwareMap);
+        color see = new color(this, telemetry, hardwareMap);
+        //lift ellie = new lift(this, telemetry, hardwareMap);
+        //revIMU gyro = new revIMU(this, telemetry, hardwareMap);
+        //found pull = new found(this, telemetry, hardwareMap);
+        grabber grabby = new grabber(this, telemetry, hardwareMap);
+        range scope = new range(this, telemetry, hardwareMap);
+        sensorColorMiddle = hardwareMap.get(ColorSensor.class,"Msensor_color_distance");
+        sensorColorFront = hardwareMap.get(ColorSensor.class,"Fsensor_color_distance");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        runtime.reset();
+        mStateTime.reset();
+        newState(State.STATE_INITIAL);  //newState(State.expample); changes state
+
+        //Execute the current state. Each STATE's case code does the following:
+        //1: Look for an EVENT that wil cause a STATE change
+        //2: If an EVENT is found, take any required ACTION, an then set the next STATE else
+        //3: If no EVENT is found, do processing for the current STATE and send TELEMETRY data for STATE
+
+
+        int skyStoneLocation = 0; //location of skystone; 0: 1st position, 1: 2nd position, 2: 3rd position
+        while (!isStopRequested() && opModeIsActive())
+        {
+            switch (mCurrentState)
+            {
+                case STATE_INITIAL:
+                    newState(State.STATE_DRIVE_TO_STONE);
+
+                    break;
+                case STATE_DRIVE_TO_STONE:
+                    vroom.driveY(40,.9,3);
+                    newState(State.STATE_LOCATE_STONE);
+                    break;
+                case STATE_LOCATE_STONE:
+                    if (see.isSkystone(sensorColorFront, 3))
+                    {
+                        skyStoneLocation = 0;
+                        vroom.driveY(-6, 1, 1);
+                        //skystone grabber code here
+                        grabby.grabSkystone(skyStoneLocation);
+                        vroom.driveY(6, 1, 1);
+                    } else if (see.isSkystone(sensorColorMiddle, 2))
+                    {
+                        skyStoneLocation = 1;
+                        vroom.driveY(-6, 1, 1);
+                        //skystone grabber code here
+                        grabby.grabSkystone(skyStoneLocation);
+                        vroom.driveY(6, 1, 1);
+                    } else
+                    {
+                        skyStoneLocation = 2;
+                        vroom.driveX(-8, 1, 1);
+                        vroom.driveY(-6, 1, 1);
+                        //skystone grabber code here
+                        grabby.grabSkystone(1);
+                        vroom.driveY(6, 1, 1);
+                        vroom.driveX(8, 1, 1);
+
+                    }
+                    newState(State.STATE_DRIVE_TO_DUMP);
+                    break;
+                case STATE_DRIVE_TO_DUMP:
+                    vroom.driveX(54, 1, 3);
+                    newState(State.STATE_DUMP);
+                    break;
+                case STATE_DUMP:
+                    //skystone grabber release code here
+                    grabby.releaseSkystone();
+                    newState(State.STATE_RETURN);
+                    break;
+                case STATE_RETURN:
+                    vroom.driveX(-54, 1, 3);
+                    newState(State.STATE_ACQUIRE_2STONE);
+                    break;
+                case STATE_ACQUIRE_2STONE:
+                    for (int i = 0; i < skyStoneLocation; i++)
+                    {
+                        vroom.driveX(-8, 1, 1);
+                    }
+                    vroom.driveY(-6, 1, 1);
+                    //skystone grabber code here
+                    grabby.grabSkystone(0);
+                    vroom.driveY(6, 1, 1);
+                    for (int i = 0; i < skyStoneLocation; i++)
+                    {
+                        vroom.driveX(8, 1, 1);
+                    }
+                    newState(State.STATE_DRIVE_TO_LAST_DUMP);
+                    break;
+                case STATE_DRIVE_TO_LAST_DUMP:
+                    vroom.driveX(54, 1, 3);
+                    //skystone grabber code here
+                    grabby.releaseSkystone();
+                    newState(State.STATE_PARK);
+                    break;
+                case STATE_PARK:
+                    vroom.driveX(-28, 1, 3);
+                    break;
+                case STATE_STOP:
+                    break;
+            }
+
+        }
+    }
+    private void newState(State newState){
+        mStateTime.reset();
+        mCurrentState = newState;
+    }
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leagueRobot/color.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.leagueRobot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Disabled
+public class color
+{
+
+
+    LinearOpMode opModeObj;
+    ColorSensor sensorColor;
+    ColorSensor sensorColorFront = null;
+    ColorSensor sensorColorMiddle = null;
+
+    public color(LinearOpMode opmode, Telemetry telemetry, HardwareMap hardwareMap){
+        opModeObj = opmode;
+        sensorColorMiddle = hardwareMap.get(ColorSensor.class,"Msensor_color_distance");
+        sensorColorFront = hardwareMap.get(ColorSensor.class,"Fsensor_color_distance");
+    }
+
+    public boolean isSkystone(ColorSensor sense, int cc){//cc is 2 for color sensor v2, and 3 for color sensor v3
+        this.sensorColor = sense;
+        try{
+            if((sense.red() * sense.green() / (sense.blue() * sense.blue()))<=cc){
+                opModeObj.telemetry.addData("skystone found", "found");
+                opModeObj.telemetry.update();
+                return true;
+            }
+            else {
+                opModeObj.telemetry.addData("skystone lost", "lost");
+                return false;
+            }
+
+        }
+        catch(ArithmeticException e){
+            if(sense.red() *sense.green() / (.00001 * .00001)<=cc){
+                opModeObj.telemetry.addData("skystone found", "found");
+                opModeObj.telemetry.update();
+                return true;
+            }
+            else {
+                opModeObj.telemetry.addData("skystone lost", "lost");
+                opModeObj.telemetry.update();
+                return false;
+            }
+        }
+    }
+
+
+}
