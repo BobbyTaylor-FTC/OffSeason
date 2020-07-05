db70e3e46e315114a37643b6b68a9c7fbd38e5c3
Robert <bobbytaylormka@gmail.com>
2020-07-05 17:10:12 -0400

    fixed basic odometry, setup for constant velocity odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OffSeason/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OffSeason/Odometry.java
@@ -18,11 +18,19 @@ public class Odometry {
 
     double leftDeadWheelPrev = 0;
     double rightDeadWheelPrev = 0;
-    double strafeDeaddWheelPrev = 0;
+    double strafeDeadWheelPrev = 0;
 
-    int leftDeadWheelCurrent = 0;
-    int rightDeadWheelCurrent = 0;
-    int strafeDeadWheelCurrent = 0;
+    double leftMMPerTick;
+    double rightMMPerTick;
+    double strafeMMPerTick;
+
+    double leftWheelDelta;
+    double rightWheelDelta;
+    double strafeWheelDelta;
+
+    double leftWheelTotal;
+    double rightWheelTotal;
+    double strafeWheelTotal;
 
     double globalPositionX = 0;
     double globalPositionY = 0;
@@ -32,9 +40,8 @@ public class Odometry {
     double fwdMovement = 0;
     double deltaTheta = 0;
 
-    public static double trackradius = 16;
+    public static double trackwidth = 16;
     public static double thetaConstant = .5;
-
 
 
 
@@ -45,26 +52,49 @@ public class Odometry {
         strafeDeadWheel = hardwareMap.get(DcMotorEx.class, "strafe_Dead");
     }
 
-    public void UpdateGlobalPosition() {
+    /**
+     *
+     * @param leftDeadWheelCurrent current position of the left deadwheel, to be passed from the opmode to allow for bulk reads
+     * @param rightDeadWheelCurrent current position of the right deadwheel, to be passed from the opmode to allow for bulk reads
+     * @param strafeDeadWheelCurrent current position of the strafe deadwheel, to be passed from the opmode to allow for bulk reads
+     */
 
-        leftDeadWheelCurrent = leftDeadWheel.getCurrentPosition();
-        rightDeadWheelCurrent = rightDeadWheel.getCurrentPosition();
-        strafeDeadWheelCurrent = strafeDeadWheel.getCurrentPosition();
+    public void UpdateGlobalPosition(int leftDeadWheelCurrent, int rightDeadWheelCurrent, int strafeDeadWheelCurrent) {
 
-        deltaTheta = (rightDeadWheelCurrent-rightDeadWheelPrev)-(leftDeadWheelCurrent-leftDeadWheelPrev)/(2*trackradius);
-        globalPositionTheta += deltaTheta;
-        perpMovement = strafeDeadWheelCurrent-strafeDeaddWheelPrev-thetaConstant*deltaTheta;
-        fwdMovement = ((leftDeadWheelCurrent-leftDeadWheelPrev)+(rightDeadWheelCurrent-rightDeadWheelPrev))/2;
+        leftWheelDelta = (leftDeadWheelCurrent-leftDeadWheelPrev)*leftMMPerTick;
+        rightWheelDelta = (rightDeadWheelCurrent-rightDeadWheelPrev)*rightMMPerTick;
+        strafeWheelDelta = (strafeDeadWheelCurrent-strafeDeadWheelPrev)*strafeMMPerTick;
 
-        globalPositionX+=fwdMovement*cos(globalPositionTheta-deltaTheta+deltaTheta/2)-perpMovement*sin(globalPositionTheta-deltaTheta+deltaTheta/2);
-        globalPositionY+=fwdMovement*sin(globalPositionTheta-deltaTheta+deltaTheta/2)+perpMovement*cos(globalPositionTheta-deltaTheta+deltaTheta/2);
+
+
+        //change in angle since the previous update
+        deltaTheta = (leftWheelDelta-rightWheelDelta)/(trackwidth);
+
+        // absolute angle
+        leftWheelTotal = leftDeadWheelCurrent*leftMMPerTick;
+        rightWheelTotal = rightDeadWheelCurrent*rightMMPerTick;
+
+
+        //changes in x and y movement
+        perpMovement = strafeWheelDelta-thetaConstant*deltaTheta;
+        fwdMovement = (leftWheelDelta+rightWheelDelta)/2.0;
+
+        //product of the rotation matrix and the [x,y] matrices
+        globalPositionX+=fwdMovement*cos(globalPositionTheta)-perpMovement*sin(globalPositionTheta);
+        globalPositionY+=fwdMovement*sin(globalPositionTheta)+perpMovement*cos(globalPositionTheta);
+
+        //storing deadwheel encoder values for future use
         leftDeadWheelPrev = leftDeadWheelCurrent;
         rightDeadWheelPrev = rightDeadWheelCurrent;
-        strafeDeaddWheelPrev = strafeDeadWheelCurrent;
+        strafeDeadWheelPrev = strafeDeadWheelCurrent;
 
+        //update theta AFTER rotation matrix has been multiplied in
+        globalPositionTheta = (leftWheelTotal-rightWheelTotal)/trackwidth;
 
-        globalPositionX+=fwdMovement*cos(deltaTheta/2)-perpMovement*sin(deltaTheta/2);
-        globalPositionY+=fwdMovement*sin(deltaTheta/2)+perpMovement*cos(deltaTheta/2);
+        opModeObj.telemetry.addData("Current X:", getGlobalPositionX());
+        opModeObj.telemetry.addData("Current Y:", getGlobalPositionY());
+        opModeObj.telemetry.addData("Current theta:", getGlobalPositionTheta());
+
     }
     public double getGlobalPositionX() {
         return globalPositionX;
